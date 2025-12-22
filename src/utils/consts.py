f949085d4c9e8184f8ca638f70d7560a8b94b04a
Artyom Matashkov <aimatashkov@mai.education>
2025-12-22 05:54:04 +0300

    Implement bucket

--- src/utils/consts.py
+++ src/utils/consts.py
@@ -1,5 +1,8 @@
 N_MUST_BE_GREATER_ZERO = "N должно быть неотрицательным числом"
 
+ALL_NUMS_MUST_BE_IN_RANGE_01 = "Все числа должны быть в диапазоне [0, 1]"
+BUCKETS_MUST_BE_POSITIVE_INT = "Кол-во блоков должно быть целым положительным числом"
+
 PARAMS_MUST_BE_INT = "Разрешены только целые числа"
-PARAMS_MUST_BE_FLOAT = "Разрешены только числа с плавающей запятой"
+PARAMS_MUST_BE_NUM = "Разрешены только числа"
 INPUT_MORE_THAN_ZERO = "Введите хотя бы одно число"
